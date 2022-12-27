dbf3445c2cefe0a51f25d7ed1163a59ccf64d85b
sbiljeek <sbiljeekdev@gmail.com>
2022-12-27 17:21:16 +0300

    Initial Commit

---
+++ TextViewIsTruncated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextViewIsTruncated
+//
+//  Created by Salman Biljeek on 12/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
